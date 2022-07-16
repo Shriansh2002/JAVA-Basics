6a8920bc2c95fd5bcc6a3f194c5d67028bb14a8c
Shriansh Agarwal <41548480+ShrianshCodingTutorials@users.noreply.github.com>
2022-07-16 18:34:14 +0530

    Java Day 1

---
+++ Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myInt = 5;
+        double myDouble = 5.35;
+        float myFloat = 5.56f;
+        long myLong = 5149571849L;
+        String myString = "Hello World!";
+
+        System.out.println(myInt);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myLong);
+        System.out.println(myString);
+    }
+}
