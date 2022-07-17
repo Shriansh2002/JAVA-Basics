fcec255b3e26806fedec1b7d7dc76f4ec0e9db16
Shriansh Agarwal <41548480+ShrianshCodingTutorials@users.noreply.github.com>
2022-07-17 12:05:06 +0530

    some changes

---
+++ OOP/I/finalKeyword.java
@@ -0,0 +1,9 @@
+package OOP.I;
+
+public class finalKeyword {
+    public static void main(String[] args) {
+        final int AGE = 10;
+        // AGE = 32; // Compile Error ⚠️
+        System.out.println(AGE);
+    }
+}
